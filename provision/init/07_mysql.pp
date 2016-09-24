ba896c905ea87984a678f15df46be162a1a17dc3
Chris Wiegman <chris@chriswiegman.com>
2016-09-24 16:10:07 -0400

    Re-add extra mysql user to prevent slowing anyone down

--- provision/init/07_mysql.pp
+++ provision/init/07_mysql.pp
@@ -37,3 +37,18 @@ mysql_database { 'tests.core.wordpress.pv':
   collate => 'utf8_general_ci',
   require => Class['mysql::server'],
 }
+
+mysql_user { 'username@localhost':
+  ensure        => 'present',
+  require       => Class['mysql::server'],
+  password_hash => mysql_password(password),
+}
+
+mysql_grant { 'username@localhost/*.*':
+  ensure     => 'present',
+  options    => ['GRANT'],
+  privileges => ['ALL'],
+  table      => '*.*',
+  user       => 'username@localhost',
+  require    => Class['mysql::server'],
+}
