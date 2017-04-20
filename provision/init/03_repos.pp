cf317de465192d8d08d3b4bc57359b7f77a0c8bb
Chris Wiegman <chris@wiegman.us>
2017-04-20 14:52:00 -0400

    Update WordPress versions.

--- provision/init/03_repos.pp
+++ provision/init/03_repos.pp
@@ -1,13 +1,13 @@
 vcsrepo { '/var/www/default-sites/wordpress/legacy/htdocs/wordpress':
   ensure   => present,
-  revision => '4.6.4',
+  revision => '4.6.5',
   provider => git,
   source   => 'https://github.com/WordPress/WordPress.git',
 }
 
 vcsrepo { '/var/www/default-sites/wordpress/stable/htdocs/wordpress':
   ensure   => present,
-  revision => '4.7.3',
+  revision => '4.7.4',
   provider => git,
   source   => 'https://github.com/WordPress/WordPress.git',
 }
