00f9794f5c3c8d5018cab0fc036ad9e785b2879a
Chris Wiegman <chris@wiegman.us>
2017-06-08 11:19:31 -0400

    Update WordPress

--- provision/init/03_repos.pp
+++ provision/init/03_repos.pp
@@ -1,13 +1,13 @@
 vcsrepo { '/var/www/default-sites/wordpress/legacy/htdocs/wordpress':
   ensure   => present,
-  revision => '4.6.6',
+  revision => '4.7.5',
   provider => git,
   source   => 'https://github.com/WordPress/WordPress.git',
 }
 
 vcsrepo { '/var/www/default-sites/wordpress/stable/htdocs/wordpress':
   ensure   => present,
-  revision => '4.7.5',
+  revision => '4.8',
   provider => git,
   source   => 'https://github.com/WordPress/WordPress.git',
 }
